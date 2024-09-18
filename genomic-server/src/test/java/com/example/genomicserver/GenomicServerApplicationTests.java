138b2d97156644da14bbe54b9f8108f3acf8614a
datnguyentien0401 <dat.nt0401@gmail.com>
2024-09-18 07:56:12 +0700

    integration backend

---
+++ genomic-server/src/test/java/com/example/genomicserver/GenomicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genomicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomicServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
